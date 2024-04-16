3a132f24dc6a65a3598783d1cb226d0fb7a459a0
tavaresnicole <tavaresnicole05@gmail.com>
2024-04-15 21:56:18 -0300

    criacao linktree barbearia dudu cortes

---
+++ script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+function next() {
+  swiper.slideNext()
+}
+
+function previous(){
+  swiper.slidePrev()
+}
