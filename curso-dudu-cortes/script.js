b1a7603267c9c9a89d7eea7be8ed8b70dc8574e1
tavaresnicole <tavaresnicole05@gmail.com>
2024-05-09 23:45:43 -0300

    ajustes no swiper

--- curso-dudu-cortes/script.js
+++ curso-dudu-cortes/script.js
@@ -1,22 +1,29 @@
 const swiper = new Swiper(".swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  
-  function next(swiperPosition) {
-  console.log(swiper);
-  swiper[swiperPosition].slideNext();
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+function next1() {
+  swiper[1].slideNext();
 }
 
-function previous(){
-    swiper[swiperPosition].slidePrev();
-  }
+function previous1() {
+  swiper[1].slidePrev();
+}
+
+function next() {
+  swiper[0].slideNext();
+}
+
+function previous() {
+  swiper[0].slidePrev();
+}
